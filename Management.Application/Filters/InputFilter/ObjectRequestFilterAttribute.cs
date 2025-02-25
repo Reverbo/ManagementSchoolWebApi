612d36bda81e32bff83e3baa5d43a1bb1680c8ba
Oberdann <oberdandeveloper@gmail.com>
2025-02-24 21:16:30 -0300

    add function filter

---
+++ Management.Application/Filters/InputFilter/ObjectRequestFilterAttribute.cs
@@ -0,0 +1,64 @@
+using FluentValidation.Results;
+using Management.Factories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Management.Filters.InputFilter.Interfaces;
+
+namespace Management.Filters.InputFilter;
+
+public class ObjectRequestFilterAttribute : ActionFilterAttribute
+{
+    private IServiceProvider _serviceProvider;
+
+    public ObjectRequestFilterAttribute(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+    }
+
+    public override void OnActionExecuting(ActionExecutingContext context)
+    {
+        var requestIs = context.HttpContext.Request.Method;
+        var objectType = context.ActionArguments.Values.Last();
+
+        if (requestIs != "POST" && requestIs != "PUT")
+        {
+            base.OnActionExecuting(context);
+            return;
+        }
+
+        if (objectType == null)
+        {
+            base.OnActionExecuting(context);
+            return;
+        }
+
+        var validatorType = typeof(IValidatorBase<>).MakeGenericType(objectType.GetType());
+
+        var validator = _serviceProvider.GetService(validatorType);
+
+        if (validator == null)
+        {
+            base.OnActionExecuting(context);
+            return;
+        }
+
+        var method = validatorType.GetMethod("Validate");
+
+        if (method == null)
+        {
+            base.OnActionExecuting(context);
+            return;
+        }
+
+        var validationResult = method.Invoke(validator, new[] { objectType }) as ValidationResult;
+
+        if (validationResult != null && !validationResult.IsValid)
+        {
+            var errorResponse = ErrorFilterAttribute.CreateObjectError(validationResult);
+            context.Result = new BadRequestObjectResult(errorResponse);
+            return;
+        }
+
+        base.OnActionExecuting(context);
+    }
+}
